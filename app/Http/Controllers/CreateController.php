1c6476abd09ce8e04aba18a69c3ec29fb8a74d9d
vishnu244 <vishnunali244@gmail.com>
2022-08-01 12:32:21 +0530

    [Vishnu]-Added @OA\Info code to all the API's to access through Swagger

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -231,6 +231,35 @@ class CreateController extends Controller
 //****************************************************************** */
 
 
+
+     /**
+     * @OA\POST(
+     *   path="/api/reset",
+     *   summary="Change Password",
+     *   description="Changing Password in Postman",
+     *   @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email","password", "newPassword"},
+     *               @OA\Property(property="email", type="email"),
+     *               @OA\Property(property="password", type="password"),
+     *               @OA\Property(property="newPassword", type="password"),
+     *               
+     *            ),
+     *        ),
+     *    ),
+     *   @OA\Response(response=200, description="password updated successfully"),
+     *   @OA\Response(response=401, description="Check your old password"),
+     *   
+     * )
+     * 
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+
 //changing password in postman
 
     public function changePassword(Request $request){
@@ -252,8 +281,33 @@ class CreateController extends Controller
     }
 
 
+     /**
+     * @OA\POST(
+     *   path="/api/forgotPassword",
+     *   summary="forgotPassword ",
+     *   description="Resetting Password using forgotPassword function",
+     *   @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email"},
+     *               @OA\Property(property="email", type="email"),          
+     *            ),
+     *        ),
+     *    ),
+     *   @OA\Response(response=200, description="Token Sent to Mail to Reset Password"),
+     *   @OA\Response(response=404, description="Email does not exists"),
+     *   
+     * )
+     * 
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+
 //--- sending token to mail to change password ------- 
-public function forgotPassword(Request $request)
+    public function forgotPassword(Request $request)
     {  
         
          $request->validate([
@@ -279,12 +333,40 @@ public function forgotPassword(Request $request)
 
             Mail::to($email)->send(new SendMail($token, $email));
             
-            return "mail sent";
+            return "Token Sent to Mail to Reset Password";
             
         }
-        
-     
+    
     }
+
+
+    /**
+     * @OA\POST(
+     *   path="/api/resetPassword",
+     *   summary="Resetting Password",
+     *   description="Resetting Password through Token",
+     *   @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email","password", "token"},
+     *               @OA\Property(property="email", type="string"),
+     *               @OA\Property(property="password", type="password"),
+     *               @OA\Property(property="token", type="string"),
+     *               
+     *            ),
+     *        ),
+     *    ),
+     *   @OA\Response(response=200, description="password Reset successfull"),
+     *   @OA\Response(response=401, description="You have entered invalid token"),
+     *   
+     * )
+     * 
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
 
     //-------------------
     public function resetPassword(Request $request)
@@ -305,10 +387,8 @@ public function forgotPassword(Request $request)
         $user->password = Hash::make($request->password);
 
         PasswordReset::where('email', $request->email)->delete();
-        return "Password changed";
-
-       
+        return "password Reset successfull";
+      
     }
-
 
 }
